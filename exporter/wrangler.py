24916abced3ad0086804d6e19415bc9518883786
obrhubr <osmiumos74@gmail.com>
2021-04-18 19:56:16 +0200

    🐛 fix bug that reads index twice

--- exporter/wrangler.py
+++ exporter/wrangler.py
@@ -5,15 +5,15 @@ import re
 def seperate_songname(string):
     split = string.split('-')
     if len(split) == 2:
-        return re.sub('[^A-Za-z0-9]+', '', split[1].lower())
+        return split[1]
     else:
-        return re.sub('[^A-Za-z0-9]+', '', split[0].lower())
+        return split[0]
 
 if __name__ == "__main__":
-    df = pd.read_csv('data/data.csv')
+    df = pd.read_csv('data/data.csv', index_col=0)
     
     df["name"] = df["name"].apply(lambda row: row.split('|')[0])
-    df["artist"] = df["name"].apply(lambda row: re.sub('[^A-Za-z0-9]+', '', row.split('-')[0].lower()))
+    df["artist"] = df["name"].apply(lambda row: row.split('-')[0])
     df["song"] = df["name"].apply(lambda row: seperate_songname(row))
 
     df.to_csv('data/wrangled.csv')
