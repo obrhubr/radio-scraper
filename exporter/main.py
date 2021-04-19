ce71f3d6c0a7d5168962140698a6881fa2823202
obrhubr <osmiumos74@gmail.com>
2021-04-19 09:54:45 +0200

    ♻ refactor the firestore to csv script to work in batches

--- exporter/main.py
+++ exporter/main.py
@@ -4,9 +4,26 @@ from firebase_admin import firestore
 import pandas as pd
 import numpy as np
 
-def export_data_to_csv(collection):
+def stream_collection_loop(collection, cursor=None):
+    limit = 100
+    while True:
+        docs = []  # Very important. This frees the memory incurred in the recursion algorithm.
+        if cursor:
+            docs = [snapshot for snapshot in
+                    collection.limit(limit).start_after(cursor).stream()]
+        else:
+            docs = [snapshot for snapshot in collection.limit(limit).stream()]
+
+        export_data_to_csv(docs)
+
+        if len(docs) == limit:
+            cursor = docs[limit-1]
+            continue
+
+        break
+
+def export_data_to_csv(docs):
     name, station, date = [], [], []
-    docs = collection.stream()
 
     for doc in docs:
         data = doc.to_dict()
@@ -15,7 +32,8 @@ def export_data_to_csv(collection):
         date.append(data["date"])
 
     df = pd.DataFrame(np.column_stack([name, station, date]), columns=['name', 'station', 'date'])
-    df.to_csv('data/data.csv')
+    with open('data/data.csv', 'a') as f:
+        df.to_csv('data/data.csv', mode='a', header=f.tell()==0, index=False)
 
 if __name__ == "__main__":
     cred = credentials.Certificate('exporter/key.json')
@@ -24,5 +42,5 @@ if __name__ == "__main__":
 
     songs = db.collection("songs")
 
-    export_data_to_csv(songs)
+    stream_collection_loop(songs)
     
