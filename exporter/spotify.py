dc68bc67d7bcf9a486797aa3c68648ab7e4ec91b
obrhubr <osmiumos74@gmail.com>
2021-04-19 09:55:31 +0200

    🐛 more accurate fetching from spotify api

--- exporter/spotify.py
+++ exporter/spotify.py
@@ -2,6 +2,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials #To access authorised Spotify data
 import pandas as pd
 import json
+import re
 
 with open('exporter/spotify.json') as json_file:
     data = json.load(json_file)
@@ -11,9 +12,24 @@ client_secret = data["client_secret"]
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager) #spotify object to access API
 
-def audio_features(track, idx):
+def audio_features(track, idx, spotify_artist, spotify_song, scraped_artist, scraped_song):
     try:
-        features = {'acousticness': 0, 'danceability': 0, 'energy': 0, 'instrumentalness': 0, 'liveness': 0, 'speechiness': 0, 'tempo': 0, 'valence': 0, 'loudness': 0, "index": 0}
+        features = {
+            'acousticness': 0, 
+            'danceability': 0, 
+            'energy': 0, 
+            'instrumentalness': 0, 
+            'liveness': 0, 
+            'speechiness': 0, 
+            'tempo': 0, 
+            'valence': 0, 
+            'loudness': 0, 
+            "index": 0,
+            'spotify_artist': "",
+            'spotify_song': "",
+            'scraped_artist': "",
+            'scraped_song': ""
+        }
 
         req = sp.audio_features(track)
         
@@ -28,21 +44,32 @@ def audio_features(track, idx):
         features['valence'] = req[0]['valence']
 
         features["index"] = idx
+
+        features["spotify_artist"] = spotify_artist
+        features["spotify_song"] = spotify_song
+
+        features["artist"] = scraped_artist
+        features["song"] = scraped_song
     except:
         return {}
 
     return features
 
 if __name__ == "__main__":
-    df = pd.read_csv('data/wrangled.csv', index_col=0)
+    df = pd.read_csv('data/wrangled.csv', index_col=0).head()
     uri = []
+    pattern = re.compile('[\W_]+')
 
     for idx, row in df.iterrows():
         print(idx, row["artist"], row["song"])
         searchResults = sp.search(q="track:" + row["song"], type="track")
         if len(searchResults["tracks"]["items"]) >= 1:
-            uri.append(audio_features(searchResults["tracks"]["items"][0]["uri"], idx))
+            for song in searchResults["tracks"]["items"]:
+                if pattern.sub('', song["album"]["artists"][0]["name"].lower()) == pattern.sub('', row["artist"].lower()):
+                    uri.append(audio_features(song["uri"], idx, song["album"]["artists"][0]["name"], song["name"], row["artist"], row["song"]))
+                    break
+                    
 
-    df = df.join(pd.DataFrame(uri), lsuffix='_caller', rsuffix='_other')
+    df = pd.merge(df, pd.DataFrame(uri), on=['artist','song'])
     df = df.dropna()
     df.to_csv('data/spotify.csv')
